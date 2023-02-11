1621d720daffd05f7273602f2e1072f2821a92f5
VictorRodriguez <victor.rodriguez.bahena@intel.com>
2023-02-11 07:29:17 -0600

    imrpove merge script
    Signed-off-by: VictorRodriguez <victor.rodriguez.bahena@intel.com>

--- detector/post_silicon/merge.py
+++ detector/post_silicon/merge.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import glob
 import os
+import sys
 
-files = os.path.join("skx_executions_emon", "*_results.csv")
+if len(sys.argv) >= 2:
+    path_ = sys.argv[1]
+else:
+    exit(-1)
+
+files = os.path.join(path_, "*_results.csv")
 
 files = glob.glob(files)
 
 df_global = pd.DataFrame()
 df = pd.read_csv(files[0])
-df_global['metric'] = df[df.columns[0]].values.tolist()
+df_global['test_name'] = df[df.columns[0]].values.tolist()
 
 for file in files:
     print(file)
@@ -17,5 +23,9 @@ for file in files:
     column_name = file.strip("./").strip(".csv")
     df_global[column_name] = results
 
-print(df_global.T)
-df_global.T.to_csv("summary.csv")
+print(df_global)
+df_ = (df_global.set_index('test_name').T)
+#df_.columns = df_.iloc[0]
+#f_ = df_.tail(-1)
+print(df_)
+df_.to_csv("summary.csv",index_label="test_name")
