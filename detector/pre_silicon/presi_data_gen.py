8151394f5f0982090630ce7b88bb82eb2f8ecd01
VictorRodriguez <victor.rodriguez.bahena@intel.com>
2023-11-13 18:31:17 -0600

    fixes for pre sili analysis
    Signed-off-by: VictorRodriguez <victor.rodriguez.bahena@intel.com>

--- detector/pre_silicon/presi_data_gen.py
+++ detector/pre_silicon/presi_data_gen.py
@@ -1,50 +1,59 @@
 #!/bin/python3
 
+import argparse
 import json
 import os
-import argparse
-import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.ticker import PercentFormatter
-from IPython.display import display
 import urllib
 import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from IPython.display import display
+from matplotlib.ticker import PercentFormatter
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
+
 
 def read_histogram(CSV_FILE):
     df = pd.read_csv(CSV_FILE)
     df.columns = df.columns.str.lower()
-    df = df.sort_values(by='count', ascending=False)
+    df = df.sort_values(by="count", ascending=False)
     return df
 
 
 def read_json(CUMULUS_ID):
-    url = f'https://cumulus-dashboard.intel.com/services-framework/api/metadata/histogram/{CUMULUS_ID}'
+    url = f"https://cumulus-dashboard.intel.com/services-framework/api/metadata/histogram/{CUMULUS_ID}"
     req = urllib.request.Request(url)
     response = urllib.request.urlopen(req)
     data = response.read()
     values = json.loads(data)
-    df = pd.json_normalize(values['data'])
+    df = pd.json_normalize(values["data"])
     df.columns = df.columns.str.lower()
     return df
 
+
 def get_cathegory(df_histogram):
-    df_intrinsics = pd.read_json('/Users/vrodri3/Downloads/intrinsics.json')
+    df_intrinsics = pd.read_json("/Users/vrodri3/Downloads/intrinsics.json")
     df_histogram["category"] = ""
 
-    for instruction in df_histogram['mnemonic']:
-        if (df_intrinsics['instruction'].eq(instruction.upper())).any():
-            category = df_intrinsics.loc[df_intrinsics['instruction'] == instruction.upper(), 'category'].iloc[0]
+    for instruction in df_histogram["mnemonic"]:
+        if (df_intrinsics["instruction"].eq(instruction.upper())).any():
+            category = df_intrinsics.loc[
+                df_intrinsics["instruction"] == instruction.upper(), "category"
+            ].iloc[0]
             category = str(category).strip()
-            df_histogram.loc[df_histogram['mnemonic'] == instruction,'category'] = category
+            df_histogram.loc[
+                df_histogram["mnemonic"] == instruction, "category"
+            ] = category
         else:
-            df_histogram.loc[df_histogram['mnemonic'] == instruction,'category'] = 'None'
+            df_histogram.loc[
+                df_histogram["mnemonic"] == instruction, "category"
+            ] = "None"
 
+    df = df_histogram.groupby(["category"]).sum(numeric_only=True)
+    df = df.sort_values(by=["count"], ascending=False)
 
-    df = df_histogram.groupby(['category']).sum(numeric_only=True)
-    df = df.sort_values(by=['count'], ascending=False)
-
-    df['percent'] = (df['count'] / df['count'].sum()) * 100
+    df["percent"] = (df["count"] / df["count"].sum()) * 100
 
     print(df)
 
@@ -52,18 +61,17 @@ def get_cathegory(df_histogram):
 
 
 def calcualte_values(df_copy):
-
     df_copy["count"] = pd.to_numeric(df_copy["count"])
     file_path = os.path.abspath(__file__)
     file_name = os.path.basename(file_path)
-    file_path = file_path.replace(file_name,"")
+    file_path = file_path.replace(file_name, "")
 
-    file_path_a = os.path.join(file_path,'instructions_kind/arithmetic.csv')
-    file_path_b = os.path.join(file_path,'instructions_kind/branch.csv')
-    file_path_s = os.path.join(file_path,'instructions_kind/store.csv')
-    file_path_l = os.path.join(file_path,'instructions_kind/load.csv')
-    file_path_v = os.path.join(file_path,'instructions_kind/vector.csv')
-    file_path_io = os.path.join(file_path,'instructions_kind/IO.csv')
+    file_path_a = os.path.join(file_path, "instructions_kind/arithmetic.csv")
+    file_path_b = os.path.join(file_path, "instructions_kind/branch.csv")
+    file_path_s = os.path.join(file_path, "instructions_kind/store.csv")
+    file_path_l = os.path.join(file_path, "instructions_kind/load.csv")
+    file_path_v = os.path.join(file_path, "instructions_kind/vector.csv")
+    file_path_io = os.path.join(file_path, "instructions_kind/IO.csv")
 
     if os.path.isfile(file_path_a):
         df_arithmetic = pd.read_csv(file_path_a)
@@ -93,7 +101,7 @@ def calcualte_values(df_copy):
     else:
         exit(0)
 
-    df_copy["pro"] = df_copy["count"]/df_copy["count"].sum()
+    df_copy["pro"] = df_copy["count"] / df_copy["count"].sum()
 
     df_a = df_copy[df_copy.mnemonic.isin(df_arithmetic.mnemonic)]
     df_b = df_copy[df_copy.mnemonic.isin(df_branch.mnemonic)]
@@ -102,75 +110,81 @@ def calcualte_values(df_copy):
     df_v = df_copy[df_copy.mnemonic.isin(df_vector.mnemonic)]
     df_io = df_copy[df_copy.mnemonic.isin(df_io.mnemonic)]
 
-    df_o = pd.concat([df_copy, df_a, df_b, df_s, df_l, df_v, df_io]).drop_duplicates(keep=False)
+    df_o = pd.concat([df_copy, df_a, df_b, df_s, df_l, df_v, df_io]).drop_duplicates(
+        keep=False
+    )
 
-    data_prob = [['arithmetic', df_a['pro'].sum()],
-                 ['branch', df_b['pro'].sum()],
-                 ['store_counter', df_s['pro'].sum()],
-                 ['load_counter', df_l['pro'].sum()],
-                 ['vector_counter', df_v['pro'].sum()],
-                 ['io_counter', df_io['pro'].sum()],
-                 ['other_counter', df_o['pro'].sum()]]
+    data_prob = [
+        ["arithmetic", df_a["pro"].sum()],
+        ["branch", df_b["pro"].sum()],
+        ["store_counter", df_s["pro"].sum()],
+        ["load_counter", df_l["pro"].sum()],
+        ["vector_counter", df_v["pro"].sum()],
+        ["io_counter", df_io["pro"].sum()],
+        ["other_counter", df_o["pro"].sum()],
+    ]
 
-    df_prob = pd.DataFrame(data_prob, columns=['InstrKind', 'probability'])
-    #print(df_prob['probability'].sum())
-    return (df_prob)
+    df_prob = pd.DataFrame(data_prob, columns=["InstrKind", "probability"])
+    # print(df_prob['probability'].sum())
+    return df_prob
+
 
 def calcualte_values_iform(df_copy):
-
     df_copy["count"] = pd.to_numeric(df_copy["count"])
     file_path = os.path.abspath(__file__)
     file_name = os.path.basename(file_path)
-    file_path = file_path.replace(file_name,"")
+    file_path = file_path.replace(file_name, "")
 
-    df_iform = read_json('presibuilds.idata_iform.json')
+    df_iform = read_json("presibuilds.idata_iform.json")
 
-    df_copy["pro"] = df_copy["count"]/df_copy["count"].sum()
+    df_copy["pro"] = df_copy["count"] / df_copy["count"].sum()
 
     df_a = df_copy[df_copy.mnemonic.isin(df_arithmetic.mnemonic)]
     df_b = df_copy[df_copy.mnemonic.isin(df_branch.mnemonic)]
     df_s = df_copy[df_copy.mnemonic.isin(df_store.mnemonic)]
     df_o = pd.concat([df_copy, df_a, df_b, df_s]).drop_duplicates(keep=False)
 
-    data_prob = [['arithmetic', df_a['pro'].sum()],
-                 ['branch', df_b['pro'].sum()],
-                 ['store_counter', df_s['pro'].sum()],
-                 ['other_counter', df_o['pro'].sum()]]
+    data_prob = [
+        ["arithmetic", df_a["pro"].sum()],
+        ["branch", df_b["pro"].sum()],
+        ["store_counter", df_s["pro"].sum()],
+        ["other_counter", df_o["pro"].sum()],
+    ]
 
-    df_prob = pd.DataFrame(data_prob, columns=['InstrKind', 'probability'])
+    df_prob = pd.DataFrame(data_prob, columns=["InstrKind", "probability"])
 
-    return (df_prob)
+    return df_prob
+
 
 def get_pareto(df):
-
-    df["cumpercentage"] = df["count"].cumsum()/df["count"].sum()*100
+    df["cumpercentage"] = df["count"].cumsum() / df["count"].sum() * 100
     df2 = pd.DataFrame(columns=["mnemonic", "count", "cumpercentage"])
     for ind in df.index:
-        if (df['cumpercentage'][ind]) <= 90:
+        if (df["cumpercentage"][ind]) <= 90:
             df2 = df2.append(df.iloc[ind])
     display(df2)
 
     fig, ax = plt.subplots()
     ax.bar(df2["mnemonic"], df2["count"], color="C0")
     ax2 = ax.twinx()
-    ax2.plot(df2["mnemonic"], df2["cumpercentage"],
-             color="C1", marker="D", ms=7)
+    ax2.plot(df2["mnemonic"], df2["cumpercentage"], color="C1", marker="D", ms=7)
     ax2.yaxis.set_major_formatter(PercentFormatter())
     ax.tick_params(axis="y", colors="C0")
-    ax.tick_params(axis="x", colors="C0",rotation=90)
+    ax.tick_params(axis="x", colors="C0", rotation=90)
     ax2.tick_params(axis="y", colors="C1")
     plt.show()
 
 
 def plot_sumary(df_sumary):
     print(df_sumary)
-    df_sumary.plot.pie(y = 'probability', \
-        labels = df_sumary['InstrKind'].tolist(), \
-        autopct='%1.1f%%',\
-        startangle=15,\
-        shadow=True
-        )
-    plt.ylabel('')
+    df_sumary.plot.pie(
+        y="probability",
+        labels=df_sumary["InstrKind"].tolist(),
+        autopct="%1.1f%%",
+        startangle=15,
+        shadow=True,
+    )
+    plt.ylabel("")
     plt.show()
 
 
@@ -179,23 +193,42 @@ def plot_stacked_bar(df):
     plt.show()
 
 
-def main():
+def compare_and_plot_totals(file1_path, file2_path):
+    df1 = pd.read_csv(file1_path)
+    df2 = pd.read_csv(file2_path)
 
+    total_count_file1 = df1.iloc[:, 1].sum()
+    total_count_file2 = df2.iloc[:, 1].sum()
+    plt.bar([file1_path, file2_path], [total_count_file1, total_count_file2])
+    plt.xlabel("Files")
+    plt.ylabel("Total Count")
+    plt.title("Comparison of Total Count between Two Files")
+    plt.show()
+
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--cumulus_uri")
     parser.add_argument("--histogram")
-    parser.add_argument('--files',nargs='*')
+    parser.add_argument("--files", nargs="*")
     args = parser.parse_args()
 
     if args.files:
-        my_labels = ['arithmetic', 'branch', 'store', 'load', 'vector' , 'io', 'other']
+        my_labels = ["arithmetic", "branch", "store", "load", "vector", "io", "other"]
         df_global = pd.DataFrame(columns=my_labels)
-        for file_name in args.files:
-            test_name = file_name.strip("csv").strip(".")
-            df = read_histogram(os.path.join("tests/pre_silicon_runs_ml_db",file_name))
+        files = os.listdir(str(args.files[0]))
+        f1 = os.path.abspath(os.path.join(str(args.files[0]), files[0]))
+        f2 = os.path.abspath(os.path.join(str(args.files[0]), files[1]))
+        compare_and_plot_totals(f1, f2)
+        for file_name in files:
+            test_name = file_name
+            df = read_histogram(os.path.join(str(args.files[0]), file_name))
             df_sumary = calcualte_values(df)
-            df_global.loc[test_name] = df_sumary['probability'].values.tolist()
+            get_pareto(df)
+            plot_sumary(df_sumary)
+            df_global.loc[test_name] = df_sumary["probability"].values.tolist()
         plot_stacked_bar(df_global)
+        exit(0)
 
     elif args.histogram:
         df = read_histogram(args.histogram)
@@ -211,5 +244,5 @@ def main():
     plot_sumary(df_sumary)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
